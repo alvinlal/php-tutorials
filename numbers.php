9df796c90a1f1b7b702c5179837899faacc1726a
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-09 23:05:47 +0530

    numbers

---
+++ numbers.php
@@ -0,0 +1,17 @@
+<?php
+
+$pi = 3.14;
+$radius = 2;
+$area = $pi * $radius;
+
+echo "The area is $area";
+
+$radius += 3;
+
+echo "\nThe radius is now $radius";
+
+echo "\nThe value of pi given by php is ", pi();
+
+echo "\nThe floored value of pi is ", floor(pi());
+
+echo "\nThe ceiled value of pi is ", ceil(pi());
