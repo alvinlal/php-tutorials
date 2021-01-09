7b9045b088174af7612fc2fd92e8ff640612b07b
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-09 15:57:21 +0530

    variables

---
+++ variables.php
@@ -0,0 +1,28 @@
+<?php
+
+define('MESSAGE2', 'world');
+
+
+$message1 = "hello";
+$name = "alvin";
+$age = 19;
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $message1,MESSAGE2 ?></h1>
+    <p><?php echo "name:",$name ?>
+    <p><?php echo "age:",$age ?>
+
+</body>
+
+</html>
