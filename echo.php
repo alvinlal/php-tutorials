64aec813210cebe6dfe3fbd7f8993ddf4f3030d2
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-09 19:05:07 +0530

    echo

---
+++ echo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php 
+   
+   echo "Hello World"."\n";
+
+   echo 'This ','string ','was ','made ','with multiple parameters. ';
+   echo 'This '.'string '.'was '.'concatenated';
+
+   
+   
+   ?>
+
+</body>
+
+</html>
