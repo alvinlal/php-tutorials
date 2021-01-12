30aedc356f32bf932310eb66ce05e04ea8103a58
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-12 19:59:34 +0530

    multidimensionalArrays

---
+++ multidimensionalArrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$blogs = [
+    ['title' => 'mario party', 'author' => 'mario', 'content' => 'lorem', 'likes' => 30],
+    ['title' => 'mario kart cheats', 'author' => 'toad', 'content' => 'lorem', 'likes' => 25],
+    ['title' => 'zelda hidden chests', 'author' => 'link', 'content' => 'lorem', 'likes' => 50],
+];
+print_r($blogs);
+
+$blogs[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
+
+$poppedBlog = array_pop($blogs);
+
+print_r($poppedBlog);
