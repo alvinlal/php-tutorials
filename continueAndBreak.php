9c919a948ad0fd8e4042bf67667f54f667fe2b12
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-13 19:30:04 +0530

    continue and break

---
+++ continueAndBreak.php
@@ -0,0 +1,26 @@
+<?php
+
+// continue and break
+
+$products = [
+    ['name' => 'shiny star', 'price' => 20],
+    ['name' => 'green shell', 'price' => 10],
+    ['name' => 'red shell', 'price' => 15],
+    ['name' => 'gold coin', 'price' => 5],
+    ['name' => 'lightning bolt', 'price' => 40],
+    ['name' => 'banana skin', 'price' => 2],
+];
+
+foreach ($products as $product) {
+
+    if ($product['name'] === 'lightning bolt') {
+        break;
+    }
+
+    if ($product['price'] > 15) {
+        continue;
+    }
+
+    echo $product['name'] . "\n";
+
+}
