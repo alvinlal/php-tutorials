a6d54ce1cbe3c3e13e047e536394294b77951d33
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-13 19:55:21 +0530

    functions

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+
+ function sayHello($name = "jane") {
+  echo "Hello {$name}";
+ }
+ sayHello("alvin");
+
+ function getlist($product) {
+  return "<h1>{$product['name']} costs {$product['price']}";
+ }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo getlist(['name' => "ps5", 'price' => '$500']); ?>
+
+</body>
+
+</html>
