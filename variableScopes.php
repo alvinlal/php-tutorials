cfb6131be34dfc2855790ded0190cd1779d75027
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-13 20:13:49 +0530

    variable scopes

---
+++ variableScopes.php
@@ -0,0 +1,30 @@
+<?php
+
+$name = 'mario';
+$name2 = 'jane';
+
+function sayHello() {
+ global $name;
+ $name = 'peach';
+ echo "hello $name";
+}
+
+sayHello();
+
+echo "\n$name";
+
+function sayHai($name) {
+ $name = 'john';
+ echo "\nHai $name";
+}
+
+sayHai($name2);
+echo "\n$name2";
+
+function sayBie(&$name) {
+ $name = 'millie';
+ echo "\nBie $name";
+}
+
+sayBie($name2);
+echo "\n$name2";
