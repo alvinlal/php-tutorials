9f597b987c9dbf9c2f505260c9efab2d8583e72b
Alvin Lal <alvinzzz2001@gmail.com>
2021-01-16 19:36:41 +0530

    ternary

---
+++ ternary.php
@@ -0,0 +1,19 @@
+<?php
+ $score = 50;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php echo $score > 40 ? 'high score!' : 'low score'; ?>
+
+</body>
+
+</html>
