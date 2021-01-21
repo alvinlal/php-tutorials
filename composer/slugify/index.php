1b642ae81d5d0b3cd79d3cea80254352a4db47cd
alvinlal <alvinzzz2001@gmail.com>
2021-01-22 00:24:12 +0530

    composer

---
+++ composer/slugify/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Cocur\Slugify\Slugify;
+
+$slugify = new Slugify();
+
+echo $slugify->slugify('Hello World, this is a long sentence and I need to make a slug from it!');
+
+?>
