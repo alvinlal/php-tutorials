e11548c2c7e11acb2c89d4f56d2fa3a503d8d580
alvinlal <alvinzzz2001@gmail.com>
2021-01-21 23:15:26 +0530

    update db password

--- ninjaPizza/config/db_connect.php
+++ ninjaPizza/config/db_connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$conn = mysqli_connect("localhost", "alvin", "test1234", "ninja_pizza");
+$conn = mysqli_connect("localhost", "alvin", "alvin123", "ninja_pizza");
 
 if (!$conn) {
- echo 'db connection error: ' . mysql_connect_error();
+	echo 'db connection error: ' . mysql_connect_error();
 }
 
 ?>
