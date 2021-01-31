998eea1ccc4d393b73f8f6441356b1fbde678705
alvinlal <alvinzzz2001@gmail.com>
2021-01-31 16:00:29 +0530

    auto load classes with composer

---
+++ autoLoadClassesWithComposer/core/Application.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\core;
+
+class Application {
+	public function __construct() {
+		echo "application created";
+	}
+}
---
+++ autoLoadClassesWithComposer/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$app = new \app\core\Application('alvin');
