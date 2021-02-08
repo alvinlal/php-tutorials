51d56baceff3dd3a20afd21169944725ddb7bf6e
alvinlal <alvinzzz2001@gmail.com>
2021-02-08 22:18:01 +0530

    Environment variables in php apache_getenv

--- phpEnvs/index.php
+++ phpEnvs/index.php
@@ -5,5 +5,9 @@ include 'vendor/autoload.php';
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-echo $_ENV['S3_BUCKET'];
+apache_setenv("TEST_ENV", "hello123");
+
+echo apache_getenv("TEST_ENV") . '</br>';
+
+echo $_ENV['S3_BUCKET'] . '</br>';
 echo $_SERVER['S3_BUCKET'];
